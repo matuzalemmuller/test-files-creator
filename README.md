49fd33a1a1ca945c95fbadad072102d4243ea36c
Matuzalem Muller <matuzalemmuller@gmail.com>
2022-04-09 22:04:47 -0700

    Add argument support
    * Add argument parsing
    * Add support for sh (instead of bash)
    * Add log file customization
    * Remove 'delete' and 'modify' functionalities

--- README.md
+++ README.md
@@ -1,34 +1,23 @@
 # Test files creator
 
-Script to be used by [crontab](https://en.wikipedia.org/wiki/Cron) to create, delete and modify test files on a scheduled basis.
+## Description
 
-## Usage
+Creates files with random content using common bash expressions and tools. The size of the files is specified in **bytes**.
 
-### Script
+### Parameters
 
+- [`REQUIRED`] -o, --output: folder where files will be saved
+- [`REQUIRED`] -s, --size: size of the files to be created
+- [`REQUIRED`] -c, --create: number of files to create
+- [`OPTIONAL`] -l, --log: log file
+- [`OPTIONAL`] --csv: log file output in csv format
+- [`OPTIONAL`] --md5: includes md5 hash in log file. Only works is csv logging is enabled
+- [`OPTIONAL`] --sha256: includes sha256 hash in log file. Only works if csv logging is enabled
+
+### Example
+
+Create 10 files in the folder 'test-folder', with 1 MB of size each.
+
+```sh
+./create.sh -o=test-folder -s=1024 -c=10
 ```
-./script.sh <folder> <file_size_KB> <files_created> <files_deleted> <files_modified> 
-```
-
-* `<folder>` is the path of where the test folder will be created and files will be saved. Default value is `$HOME/test`.
-* `<file_size_KB>` is the size of the files that will be created, in KB. Default value is `50000` (which is 50MB).
-* `<files_created>` is the number of files that will be created. Default value is `5`.
-* `<files_deleted>` is the number of files that will be deleted. Default value is `0`.
-* `<files_modified>` is the number of files that will be modified. Default value is `0`.
-
-
-### Crontab
-
-Check the [docs](https://linux.die.net/man/1/crontab) for instructions on how to use `crontab`. 
-
-### Example of usage
-
-Crontab file:
-```
-0 * * * * $HOME/scripts/script.sh $HOME/tests 10 2 8
-```
-
-Script from folder `$HOME/scripts/script.sh` will be run hourly.
-* 10 test files will be created at `$HOME/tests`.
-* 2 files from `$HOME/tests` will be deleted.
-* 8 files from `$HOME/tests` will be modified.
---
+++ create.sh
@@ -0,0 +1,202 @@
+#!/usr/bin/env sh
+
+###############################################################################
+#
+# Author : Matuzalem (Mat) Muller dos Santos
+# URL    : https://github.com/matuzalemmuller/test-files-creator
+#
+# Description:
+#   Creates files with random content using common bash expressions and tools.
+#   The size of the files is specified in **bytes**.
+#
+# Parameters:
+#   - [REQUIRED] -o, --output: folder where files will be saved
+#   - [REQUIRED] -s, --size: size of the files to be created
+#   - [REQUIRED] -c, --create: number of files to create
+#   - [OPTIONAL] -l, --log: log file
+#   - [OPTIONAL] --csv: log file output in csv format
+#   - [OPTIONAL] --md5: includes md5 hash in log file. Only works is csv
+#                       logging is enabled
+#   - [OPTIONAL] --sha256: includes sha256 hash in log file. Only works if csv
+#                          logging is enabled
+#
+# Example:
+#   Create 10 files in the folder 'test-folder', with 1 MB of size each.
+#   ./create.sh -o=test-folder -s=1024 -c=10
+###############################################################################
+
+# Parses arguments. Based on https://stackoverflow.com/a/14203146
+for i in "$@"; do
+  case $i in
+    -o=*|--output=*)
+      OUTPUT_FOLDER="${i#*=}"
+      shift # past argument=value
+      ;;
+    -o|--output)
+      echo "Missing parameter for --output"
+      exit 1
+      ;;
+    -s=*|--size=*)
+      FILE_SIZE="${i#*=}"
+      shift # past argument=value
+      ;;
+    -s|--size)
+      echo "Missing parameter for --size"
+      exit 1
+      ;;
+    -c=*|--create=*)
+      CREATE="${i#*=}"
+      shift # past argument=value
+      ;;
+    -c|--c)
+      echo "Missing parameter for --create"
+      exit 1
+      ;;
+    -l=*|--log=*)
+      LOG_FILE="${i#*=}"
+      LOG_FORMAT=TXT
+      shift # past argument=value
+      ;;
+    -l|--log)
+      echo "Missing parameter for --log"
+      exit 1
+      ;;
+    -csv)
+      LOG_FORMAT=CSV;
+      shift
+      ;;
+    -sha256)
+      HASH_ALG=sha256
+      shift
+      ;;
+    -md5)
+      HASH_ALG=md5
+      shift
+      ;;
+    -v|--verbose)
+      VERBOSE=YES
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+# Verbose output
+if [ "$VERBOSE" = "YES" ]; then
+  echo "-------------------------------------------------------------"
+  echo "OUTPUT FOLDER    = ${OUTPUT_FOLDER}"
+  echo "SIZE (BYTES)     = ${FILE_SIZE}"
+  echo "CREATE # FILES   = ${CREATE}"
+  echo "LOG FILE         = ${LOG_FILE}"
+  echo "LOG FORMAT       = ${LOG_FORMAT}"
+  echo "HASH_ALG         = ${HASH_ALG}"
+  echo "-------------------------------------------------------------"
+fi
+
+########################### ARGUMENT VALIDATION ###############################
+
+
+# Check if file size is integer and greater than 0
+if ! [ "$FILE_SIZE" -gt 0 > /dev/null 2>&1 ]; then
+  echo "Invalid input argument for --size"
+  exit 1
+fi
+
+# Check if number of files to be created is integer and greater than 0
+if ! [ "$CREATE" -gt 0 > /dev/null 2>&1 ]; then
+  echo "Invalid input argument for --create"
+  exit 1
+fi
+
+# Check if argument for folder output was provided
+if ! [ -n "$OUTPUT_FOLDER" ]; then
+  echo "Missing argument --output"
+  exit 1
+fi
+
+# Checks if folder where files will be created exists
+# If folder doesn't exist, creates folder
+if [ ! -d "$OUTPUT_FOLDER" ]; then
+  if [ ! -L "$OUTPUT_FOLDER" ]; then
+    mkdir "$OUTPUT_FOLDER"
+  fi
+fi
+
+# Updates variable value to use absolute path (useful to log file path)
+OUTPUT_FOLDER=$(realpath ${OUTPUT_FOLDER})
+
+# Create log file
+if [ -n "$LOG_FILE" ]; then
+  touch ${LOG_FILE} > /dev/null 2>&1
+  if ! [ -f ${LOG_FILE} ]; then
+    echo "Log output cannot be saved at ${LOG_FILE}"
+    exit 1
+  fi
+
+  # Checks if hash program is installed if hash logging is enabled
+  if [ -n "$HASH_ALG" ]; then
+    if [ "$LOG_FORMAT" = "CSV" ]; then
+      if [ "$HASH_ALG" = "md5" ]; then
+        if [ -z $(command -v md5sum) ]; then
+          echo "Can't log hash because md5sum is not installed"
+          exit 1
+        fi
+      elif [ "$HASH_ALG" = "sha256" ]; then
+        if [ -z $(command -v sha256sum) ]; then
+          echo "Can't log hash because sha256sum is not installed"
+          exit 1
+        fi
+      fi
+    else
+      echo "Hash argument detected but no csv argument. Falling back to text log"
+    fi
+  fi
+fi
+
+###############################################################################
+
+# Creates test files
+while [ $CREATE -gt 0 ]; do
+  # If uuidgen is not available, names files based on date and random
+  if [ -z $(command -v uuidgen) ]; then
+    DATE=$(date +%s)
+    FILE_NAME="$(($DATE + $RANDOM))$RANDOM"
+  else
+    FILE_NAME=$(uuidgen)
+  fi
+
+  # Creates file
+  dd if=/dev/urandom of=$OUTPUT_FOLDER/$FILE_NAME bs=1024 count=$FILE_SIZE status=none
+
+  # Verbose output
+  if [ "$VERBOSE" = "YES" ]; then
+    echo "Created $OUTPUT_FOLDER/$FILE_NAME"
+  fi
+
+  # Logs file creation
+  if [ -n "$LOG_FILE" ]; then
+    # Calculates hash
+    if [ -n "$HASH_ALG" ]; then
+      if [ "$HASH_ALG" = "md5" ]; then
+        hash=$(md5sum ${OUTPUT_FOLDER}/${FILE_NAME} | awk '{ print $1 }')
+        hash=";${hash}"
+      elif [ "$HASH_ALG" = "sha256" ]; then
+        hash=$(sha256sum ${OUTPUT_FOLDER}/${FILE_NAME} | awk '{ print $1 }')
+        hash=";${hash}"
+      fi
+    fi
+    # Saves log to file
+    if [ "$LOG_FORMAT" = "TXT" ]; then
+      date +"%H:%M:%S %D - CREATE - $FILE_SIZE KB - $OUTPUT_FOLDER/$FILE_NAME" >> $LOG_FILE
+    elif [ "$LOG_FORMAT" = "CSV" ]; then
+      date +"%H:%M:%S %D;CREATE;$FILE_SIZE KB;$OUTPUT_FOLDER/$FILE_NAME${hash}" >> $LOG_FILE
+    fi
+  fi
+
+  CREATE=$(($CREATE-1))
+done
